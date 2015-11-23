9a89f0cc8ede216dab165cb015402620d86d78ad
alu0100221879 <alu0100221879@ull.edu.es>
2015-11-23 20:04:08 +0000

    Creada estructura de la gema con Bundler (Rake, RSpec, Guard, Travis-CI)

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lenguajes y Paradigmas de la Programación #
+**Práctica 8**: Programación Orientada a Objetos: Módulos
---
+++ spec/dLinkedList_spec.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+require "spec_helper"
+require "dLinkedList"
+
+describe DLinkedList do
+    
+    before :each do
+        
+    end
+    
+end
